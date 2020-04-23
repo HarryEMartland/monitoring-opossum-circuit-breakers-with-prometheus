b242eb8f1b4b58dfb60c0c4cf218c766d97a0fd9
martlandh <HarryMartland@Rentalcars.com>
2020-04-23 16:35:26 +0100

    upgraded oposum-prometheus and started to collect default metrics

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const {register} = require('prom-client');
+const {register, collectDefaultMetrics} = require('prom-client');
 const PrometheusMetrics = require('opossum-prometheus');
-const prometheusMetrics = new PrometheusMetrics(register)
+const prometheusMetrics = new PrometheusMetrics({registry: register})
+collectDefaultMetrics();
 
 const indexRouter = require('./routes/index');
 const errorRouter = require('./routes/error');
@@ -20,28 +21,28 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter(errorService));
 app.use('/error', errorRouter);
-app.get('/metrics', (req, res)=>res.end(register.metrics()))
+app.get('/metrics', (req, res) => res.end(register.metrics()))
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
